e325ce93a8a6ba2162747f7bb61b0ec2b375c447
Bimal Kishor Sahu <81922223+bimalsahu@users.noreply.github.com>
2023-08-21 02:13:39 +0530

    Add files via upload

---
+++ Employee Management System/process/dbh.php
@@ -0,0 +1,14 @@
+dgxxcvb<?php
+
+$servername = "localhost";
+$dBUsername = "root";
+$dbPassword = "";
+$dBName = "ems";
+
+$conn = mysqli_connect($servername, $dBUsername, $dbPassword, $dBName);
+
+if(!$conn){
+	echo "Databese Connection Failed";
+}
+
+?>
